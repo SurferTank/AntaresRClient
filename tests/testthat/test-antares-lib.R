0e3b1a6f79f2b2b702040735a800333689c2c053
Leonardo Javier Belen <leobelen@gmail.com>
2017-11-07 16:39:13 -0300

    small changes

--- tests/testthat/test-antares-lib.R
+++ tests/testthat/test-antares-lib.R
@@ -8,17 +8,16 @@ test_that("message sends", {
 })
 
 testthat("nude test", {
-  test.message <- "{\"action\":\"create\",\"documents\":[{\"type\":\"account_form\",\"post_date\":\"2001-01-01T00:00:00+3\",\"create_summary\":false,\"header\":{\"period\":2001},\"fields\":{\"someVariableOnMessage\":100.1}}]}"
-  token = "96ded62c88a8bf28bab650ddde98b3f292d17892"
+  test.message <- "{\"content\":{\"action\":\"create\",\"documents\":[{\"type\":\"account_form\",\"post_date\":\"2001-01-01T00:00:00+3\",\"create_summary\":false,\"header\":{\"period\":2001},\"fields\":{\"someVariableOnMessage\":100.1}}]}}"
 
   url = "http://localhost:8000/antares/msg/api/message/"
 
 
   RCurl::postForm(url,
-           .opts = list(postfields = test.message,
-                        httpheader = c('Content-Type' = 'application/json',
+             .opts = list(postfields = test.message, httpheader = c('Content-Type' = 'application/json',
                                        Accept = 'application/json',
                                        "Authorization"=paste("Token ", token))))
+
 
 
 })
